28d16f7d806479169bb52b598d2c684171c0d104
Arkasha <ivanovrkasha@gmail.com>
2019-02-03 17:02:21 +0300

    adding support avr

--- include/sysemu/arch_init.h
+++ include/sysemu/arch_init.h
@@ -23,6 +23,7 @@ enum {
     QEMU_ARCH_MOXIE = (1 << 15),
     QEMU_ARCH_TRICORE = (1 << 16),
     QEMU_ARCH_NIOS2 = (1 << 17),
+    QEMU_ARCH_AVR = (1 << 18),
     QEMU_ARCH_HPPA = (1 << 18),
     QEMU_ARCH_RISCV = (1 << 19),
 };
